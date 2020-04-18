cdea6931d89b971d29cde65f5a1f3c08c11db057
malinghan <linghan.ma@gmail.com>
2020-04-18 19:58:38 +0800

    add log

--- Chapter 3/mybatis-pagehelper-demo/src/main/java/geektime/spring/data/mybatisdemo/mapper/CoffeeMapper.java
+++ Chapter 3/mybatis-pagehelper-demo/src/main/java/geektime/spring/data/mybatisdemo/mapper/CoffeeMapper.java
@@ -5,11 +5,10 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.session.RowBounds;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
+@Mapper
 public interface CoffeeMapper {
     @Select("select * from t_coffee order by id")
     List<Coffee> findAllWithRowBounds(RowBounds rowBounds);
